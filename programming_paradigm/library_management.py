6c53fa7e45c292497dd9ff960bdd6a456703da3c
Kirui Allan Cheruiyot <allankirui17@gmail.com>
2025-08-03 22:35:35 +0300

    Implement basic OOP for a library management system

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,64 @@
+class Book:
+    """A class to represent a book in the library."""
+
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        """Mark the book as returned."""
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        """Check if the book is available for checkout."""
+        return not self._is_checked_out
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    """A class to manage a collection of books."""
+
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        """Add a new book to the library."""
+        self._books.append(book)
+
+    def check_out_book(self, title):
+        """Check out a book by its title."""
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return
+        print(f"'{title}' is not available for checkout.")
+
+    def return_book(self, title):
+        """Return a book by its title."""
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return
+        print(f"'{title}' was not checked out or not found.")
+
+    def list_available_books(self):
+        """List all books that are currently available."""
+        available = [book for book in self._books if book.is_available()]
+        if not available:
+            print("No books are currently available.")
+        else:
+            for book in available:
+                print(book)
